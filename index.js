69fa56e3f0438ec8ffe772f671703aeaf15f9d0e
Rejuwan12 <rezwanrian1@gmail.com>
2024-12-13 23:58:48 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 const express = require('express')
-const cors = require('cors')
-require('dotenv').config();
-const app =express();
-const port = process.env.PORT || 4000
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000
 
-// middleware
+
+//middleware
 app.use(cors())
 app.use(express.json())
 
-//mongodb
 
+const url = `mongodb+srv://gedget-shop:zkIMABBBisHzuy3O@cluster0.anrbjpf.mongodb.net/<databaseName>?retryWrites=true&w=majority`;
 
-
-
-//api
-
-app.get('/',(req,res)=>{
-    res.send('server is running')
+const client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1, // Add this line
 });
 
-app.listen(port,()=>{
-    console.log(`server is runnung on ${port}`)
-})
+
+
+
+// MongoDB connection function
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB successfully');
+    } catch (err) {
+        console.error('Error connecting to MongoDB:', err);
+    }
+  }
+  
+  // Call the function to connect to the database
+  connectToDatabase();
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
